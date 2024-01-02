c5ecab8c9b3b48eb61696dec1629530a3e0b0252
kareenabhade <kareenabhade16@gmail.com>
2024-01-02 22:01:03 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import {Routes, Route, Navigate} from "react-router-dom"
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Chat() {
+  return (
+    <div>Chat Chat Chat!</div>
+  )
+}
+
+export default Chat
