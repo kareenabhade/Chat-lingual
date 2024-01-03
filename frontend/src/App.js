6557b3550a83a434f3151569e4207491185abab7
kareenabhade <kareenabhade16@gmail.com>
2024-01-03 11:49:48 +0530

    adding Socket.io folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
+import "./index.css";
 import {Routes, Route, Navigate} from "react-router-dom"
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import {Container} from "@mui/material";
 
 function App() {
   return (
     <>
+    <Container fixed >
     <Routes>
       <Route path="/" element={<Chat />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
+    </Container>
     
     </>
   );
